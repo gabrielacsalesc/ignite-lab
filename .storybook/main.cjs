3a63c20e63393b3f7251b374e7f018132e251f71
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-10-12 22:26:45 -0300

    ci: add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,6 +15,7 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
+  
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-system/'
